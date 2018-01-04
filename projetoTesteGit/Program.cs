ca4acba05750e9d4855a5b4200dd277e5dfe4856
Felipe Farias <felipesouzaf11@gmail.com>
2018-01-04 14:21:55 -0200

    Acrescentando mensagem 'Seja bem vindo' e um laco for

--- projetoTesteGit/Program.cs
+++ projetoTesteGit/Program.cs
@@ -5,6 +5,11 @@ namespace projetoTesteGit {
         static void Main(string[] args) {
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Bom Dia!");
+            Console.WriteLine("Seja bem-vindo!");
+
+            for(int i = 0; i < 10; i++) {
+                Console.WriteLine("Olá!");
+            }
 
             Console.ReadLine();
         }
